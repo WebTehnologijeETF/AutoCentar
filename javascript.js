d91b809c7fe33769a2002e17a80c4e27dc2dc2d3
Ajdin Mehic <amehic3@etf.unsa.ba>
2015-04-08 00:28:23 +0200

    izmjena8

---
+++ javascript.js
@@ -0,0 +1,19 @@
+ function prikaziMeni(){
+   document.getElementById("padajuci").style.display="block";
+ }
+ function sakrijMeni(){
+   document.getElementById("padajuci").style.display="none";
+ }
+ 
+ function oboji(obj){	
+     obj.style.backgroundColor = "#FFB2B2";  
+ }
+ function vratiBoju(obj){
+	 obj.style.backgroundColor="white";   
+	
+ }
+ 
+ function funkcija(boja){
+	 
+	 getElementById("oznaka").style.backgroundColor=boja;
+ }
